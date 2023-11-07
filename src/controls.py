fe1b1b181e804b6181d6a0d382444c8091b1831d
AndreasK <andreas.kirschner@gmail.com>
2023-11-07 21:08:35 +0100

    power amplifyer

--- src/controls.py
+++ src/controls.py
@@ -1,17 +1,19 @@
 import datetime
 import traceback
-from gpiozero import Button
+from gpiozero import Button, DigitalOutputDevice
 from apscheduler.schedulers.background import BackgroundScheduler
 from domain import AlarmClockState
 
-button1 = 23
-button2 = 24
-button3 = 12
-button4 = 16
+button1Id = 23
+button2Id = 24
+button3Id = 12
+button4Id = 16
+audioPowerPinId = 14
 
 class Controls:
 	refreshScheduler = BackgroundScheduler()
 	buttons = []
+	powerAudioPin: DigitalOutputDevice
 	state: AlarmClockState
 
 	def __init__(self, state: AlarmClockState) -> None:
@@ -35,6 +37,10 @@ class Controls:
 	def button4Action(self):
 		self.state.audioState.toggleStream()
 
+	def hardwareButton4Action(self):
+		self.powerAudioPin.on()
+		self.button4Action()
+		self.powerAudioPin.off()
 
 	def configureKeyboard(self):
 		def keyPressedAction(key):
@@ -58,10 +64,10 @@ class Controls:
 
 	def configureGpio(self):
 		for button in ([
-			dict(b=button1, ht=0.5, hr=True, wa=self.button1Action, wh=self.button1Action), 
-			dict(b=button2, ht=0.5, hr=True, wa=self.button2Action, wh=self.button2Action), 
-			dict(b=button3, wa=self.button3Action), 
-			dict(b=button4, wa=self.button4Action)
+			dict(b=button1Id, ht=0.5, hr=True, wa=self.button1Action, wh=self.button1Action), 
+			dict(b=button2Id, ht=0.5, hr=True, wa=self.button2Action, wh=self.button2Action), 
+			dict(b=button3Id, wa=self.button3Action), 
+			dict(b=button4Id, wa=self.hardwareButton4Action)
 			]):
 			b = Button(button['b'])
 			if ('ht' in button): b.hold_time=button['ht']
@@ -69,6 +75,8 @@ class Controls:
 			if ('wh' in button): b.when_held = button['wh']
 			if ('wa' in button): b.when_activated = button['wa']
 			self.buttons.append(b)
+		powerAudioPin = DigitalOutputDevice(audioPowerPinId)
+		powerAudioPin.off()
 
 	def updateClock(self):
 		print ("update clock")
