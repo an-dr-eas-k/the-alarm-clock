f0200d5332a5726621cdf0841d3a3523ede13827
AndreasK <andreas.kirschner@gmail.com>
2023-11-16 20:55:07 +0100

    exception handling

--- src/display.py
+++ src/display.py
@@ -22,31 +22,31 @@ class Display(Observer):
 
 	def update(self, observation: Observation):
 		super().update(observation)
-		self.adjust_display()
+		try:
+			self.adjust_display()
+		except Exception as e:
+			print(e)
 					
 	def adjust_display(self):
+		
+		font=ImageFont.truetype(self.font_file, 40)
+		font_BBox = font.getbbox(self.content.clock)
+		width = font_BBox[2] - font_BBox[0]
+		height = font_BBox[3] - font_BBox[1]
 		with canvas(self.device) as draw:
-			print ("write text to display")
-			draw.text((20, 20), "Hello World!", fill="white")
-
-		# font=ImageFont.truetype(self.font_file, 40)
-		# font_BBox = font.getbbox(self.content.clock)
-		# width = font_BBox[2] - font_BBox[0]
-		# height = font_BBox[3] - font_BBox[1]
-		# with canvas(self.device) as draw:
-		# 	x = (draw.im.size[0]-width)/2
-		# 	y = (draw.im.size[1]-height)/2
+			x = (draw.im.size[0]-width)/2
+			y = (draw.im.size[1]-height)/2
  
-		# 	wifi = ImageOps.invert( Image.open( self.no_wifi_file ))
-		# 	draw.bitmap([10,10], wifi
-		# 					 .resize([int(0.05 * s) for s in wifi.size]), fill=1 )
-		# 	draw.text(
-		# 		stroke_width=0, 
-		# 		fill=1,
-		# 		align='left',
-		# 		text=self.content.clock,
-		# 		xy=[x, y],
-		# 		font=font)
+			wifi = ImageOps.invert( Image.open( self.no_wifi_file ))
+			draw.bitmap([10,10], wifi
+							 .resize([int(0.05 * s) for s in wifi.size]), fill=1 )
+			draw.text(
+				stroke_width=0, 
+				fill=1,
+				align='left',
+				text=self.content.clock,
+				xy=[x, y],
+				font=font)
 
 
 if __name__ == '__main__':
@@ -69,6 +69,6 @@ if __name__ == '__main__':
 		draw.text((20, 20), "Hello World!", fill="white")
 
 	if is_on_hardware:
-		time.sleep(120)
+		time.sleep(10)
 	else:
 		dev.image.save("foo.png", format="png")
