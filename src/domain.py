b8bc16a8cc3ff610bdc8bc93afc89b01f039d56e
AndreasK <andreas.kirschner@gmail.com>
2023-12-17 17:54:52 +0100

    min brightness

--- src/domain.py
+++ src/domain.py
@@ -102,7 +102,7 @@ class DisplayContent(Observable):
 	is_volume_meter_shown: bool= False
 	show_blink_segment:bool= True
 	contrast: int= 0
-	grayscale_intensity: int= 255
+	grayscale_intensity: int= 16
 
 	@property
 	def clock(self) -> str:
