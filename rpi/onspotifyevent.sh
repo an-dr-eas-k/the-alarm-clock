554b918ef9cb32d85e37f78ee487e687ef0561fa
AndreasK <andreas.kirschner@gmail.com>
2024-03-02 20:05:37 +0100

    exit 0

--- rpi/onspotifyevent.sh
+++ rpi/onspotifyevent.sh
@@ -4,3 +4,5 @@ cd /srv/the-alarm-clock
 python -u src/app_librespotify_event_listener.py \
         2>> /var/log/the-alarm-clock.spotify-event.errout \
         1>> /var/log/the-alarm-clock.spotify-event.stdout
+
+exit 0
