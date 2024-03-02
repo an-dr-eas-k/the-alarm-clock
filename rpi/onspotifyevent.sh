863e110ec678758540b748f5fde38dc5d6e4c23d
AndreasK <andreas.kirschner@gmail.com>
2024-03-02 15:20:40 +0100

    update scripts

--- rpi/onspotifyevent.sh
+++ rpi/onspotifyevent.sh
@@ -2,5 +2,5 @@
 
 cd /srv/the-alarm-clock
 python -u src/app_librespotify_event_listener.py \
-        2> /var/log/the-alarm-clock.spotify-event.errout \
-        1> /var/log/the-alarm-clock.spotify-event.stdout
+        2>> /var/log/the-alarm-clock.spotify-event.errout \
+        1>> /var/log/the-alarm-clock.spotify-event.stdout
