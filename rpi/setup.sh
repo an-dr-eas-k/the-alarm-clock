15138653a0b8c5acf22d57cc9f2f7a347102ed4c
AndreasK <andreas.kirschner@gmail.com>
2024-04-16 22:24:00 +0200

    clone project

--- rpi/setup.sh
+++ rpi/setup.sh
@@ -19,24 +19,25 @@ EOF
 
 # add and configure the-alarm-clock user
 uid=1010
+uhome=/srv/the-alarm-clock
 addgroup --system --gid $uid the-alarm-clock
-adduser --system --home /srv/the-alarm-clock/ --uid $uid --gid $uid --disabled-password the-alarm-clock
+adduser --system --home $uhome --uid $uid --gid $uid --disabled-password the-alarm-clock
 adduser the-alarm-clock gpio
 adduser the-alarm-clock i2c
 adduser the-alarm-clock spi
-mkdir -p /srv/the-alarm-clock
-chown $uid:$uid -R /srv/the-alarm-clock
+mkdir -p $uhome
+chown $uid:$uid -R $uhome
 
 ln -s ~+/resources/sudoers /etc/sudoers.d/the-alarm-clock
 
 # setup raspotify
 
 ln -s ~+/resources/raspotify.service /lib/systemd/system/raspotify.service
-ln -s ~+/resources/asoundrc /srv/the-alarm-clock/.asoundrc
+ln -s ~+/resources/asoundrc $uhome/.asoundrc
 ln -s ~+/resources/raspotify.conf /etc/raspotify/conf
 
 cat >> /etc/raspotify/conf << "EOF"
-LIBRESPOT_ONEVENT="/srv/the-alarm-clock/rpi/onspotifyevent.sh"
+LIBRESPOT_ONEVENT="${uhome}/rpi/onspotifyevent.sh"
 EOF
 
 
@@ -48,7 +49,12 @@ uid=1010
 if [ -z "$( grep the-alarm-clock /etc/rc.local )" ]; then
 	sed -i '/exit/d' /etc/rc.local
 	cat >> /etc/rc.local << "EOF"
-sudo -u the-alarm-clock -- bash /srv/the-alarm-clock/rpi/onboot.sh 2>> /var/log/the-alarm-clock.errout 1>> /var/log/the-alarm-clock.stdout &
+sudo -u the-alarm-clock -- bash ${uhome}/rpi/onboot.sh 2>> /var/log/the-alarm-clock.errout 1>> /var/log/the-alarm-clock.stdout &
 exit 0
 EOF
 fi
+
+
+git clone https://github.com/an-dr-eas-k/the-alarm-clock.git $uhome
+cd $uhome
+git checkout develop
