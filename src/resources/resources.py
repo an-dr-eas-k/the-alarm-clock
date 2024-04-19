3d81915d57ffbd250bb7ee0ce499df04d260e01e
AndreasK <andreas.kirschner@gmail.com>
2024-04-19 20:20:51 +0200

    update to search for soundcard also

--- src/resources/resources.py
+++ src/resources/resources.py
@@ -16,6 +16,7 @@ librespotify_env_vars = ["PLAYER_EVENT", "TRACK_ID", "OLD_TRACK_ID", "DURATION_M
 alarm_details_file = f"/tmp/alarm_details.json"
 
 valid_mixer_device_simple_control_names = ["Digital","Master"]
+valid_sound_card_pattern = ["digiamp","ALC257"]
 
 def init_logging():
 	logging.config.fileConfig(os.path.join(resources_dir, 'logging.conf'))
--- src/utils/os.py
+++ src/utils/os.py
@@ -3,6 +3,7 @@ import os
 import re
 import subprocess
 from resources.resources import valid_mixer_device_simple_control_names as valid_mixers
+from resources.resources import valid_sound_card_pattern as valid_cards
 
 logger = logging.getLogger("tac.os")
 
@@ -27,9 +28,10 @@ def shutdown_system():
 
 def get_system_volume(control_name: str = None) -> float:
 	logger.debug("getting system volume")
+	card_id = get_system_sound_card_id()
 	if control_name is None:
-		control_name = get_system_volume_control_name()
-	output = subprocess.check_output(["amixer", "sget", control_name])
+		control_name = get_system_volume_control_name(card_id)
+	output = subprocess.check_output(["amixer", "sget", "-c", card_id, control_name])
 	lines = output.decode().splitlines()
 	pattern = r"\[(\d+)%\]"
 	volumes = [ float(re.search(pattern, line).group(1)) for line in lines if re.search(pattern, line)]
@@ -39,13 +41,24 @@ def get_system_volume(control_name: str = None) -> float:
 
 def set_system_volume(newVolume: float):
 	logger.debug("setting system volume to %s" % newVolume)
-	control_name = get_system_volume_control_name()
+	control_name = get_tac_volume_control()
 	subprocess.call(["amixer", "sset", control_name, f"{newVolume * 100}%"], stdout=subprocess.DEVNULL)
 
-def get_system_volume_control_name():
+def get_system_sound_card_id():
+	pattern = rf"^card (\d+):"
+	output = subprocess.check_output(["aplay", "-l"]).decode().splitlines()
+	sound_card_id = next(re.search(pattern, line).group(1) for line in output if any([re.search(pattern_entry, line) for pattern_entry in valid_cards]))
+	logger.debug(f"sound card id: {sound_card_id}")
+	return sound_card_id
+
+def get_tac_volume_control():
+	card_id = get_system_sound_card_id()
+	return get_system_volume_control_name(card_id)
+
+def get_system_volume_control_name(sound_card_id: int):
 	pattern = r"^Simple.+'(.+)',\d+$"
-	output = subprocess.check_output(["amixer", "scontrols"])
-	available_mixers = [re.match(pattern, line).group(1) for line in output.decode().splitlines() if line.startswith("Simple")]
+	output = subprocess.check_output(["amixer", "scontrols", "-c", sound_card_id]).decode()
+	available_mixers = [re.search(pattern, line).group(1) for line in output.splitlines() if line.startswith("Simple")]
 	control_name = next((control_name for control_name in available_mixers if control_name in valid_mixers), None)
 	logger.debug(f"volume mixer control name: {control_name}")
 	return control_name
