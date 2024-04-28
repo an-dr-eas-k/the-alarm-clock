8f31846f6e0641d49f05a4387016a93989fce13f
AndreasK <andreas.kirschner@gmail.com>
2024-04-28 17:40:20 +0200

    update formula

--- case/simple.py
+++ case/simple.py
@@ -4,9 +4,6 @@ a = 60
 d = 65
 epsilon : float = 75
 
-c = lambda a, d, epsilon: 1 / math.tan(math.radians(epsilon)) * ( d + ((a/math.cos(math.radians(epsilon)) ) ** 2 - a ** 2) ** (1/2) )
-
-b1 = lambda a, c, epsilon: (c-a)/math.cos(math.radians(epsilon))
-b = lambda a, d, epsilon: b1(a, c(a, d, epsilon), epsilon)
-
+c = lambda a, d, epsilon: a + d/math.tan(math.radians(epsilon))
+b = lambda a, d, epsilon: d/math.sin(math.radians(epsilon))
 print ("a = %d, b = %.2f, c = %.2f, d = %d, epsilon = %.2f" % (a, b(a, d, epsilon), c(a, d, epsilon), d, epsilon))
