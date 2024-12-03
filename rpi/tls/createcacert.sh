e12a2910d7dcc29b899970370c63d632d9e17aea
AndreasK <andreas.kirschner@gmail.com>
2024-12-03 22:20:55 +0100

    update cert generation

--- rpi/tls/createcacert.sh
+++
@@ -1,3 +0,0 @@
-npx mkcert create-ca --organization andreask --country-code DE --state Bavaria --locality Munich --validity 30000
-
-npx mkcert create-cert --validity 30000 --domains the-alarm-clock,the-alarm-clock.fritz.box,localhost
