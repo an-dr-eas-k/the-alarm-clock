0d0fa27fed19b6f3483611d9f3a665b841b3b153
AndreasK <andreas.kirschner@gmail.com>
2025-09-21 21:09:16 +0200

    change to int

--- src/core/infrastructure/mcp23017/rotary_encoder.py
+++ src/core/infrastructure/mcp23017/rotary_encoder.py
@@ -22,12 +22,12 @@ class RotaryEncoderManager:
         )
 
     def _pin_callback(self, mcp, pin):
-        channel_a_value = not mcp.get_pin(rotary_encoder_channel_a).value
-        channel_b_value = not mcp.get_pin(rotary_encoder_channel_b).value
+        channel_a_value = int(not mcp.get_pin(rotary_encoder_channel_a).value)
+        channel_b_value = int(not mcp.get_pin(rotary_encoder_channel_b).value)
 
         state = (channel_a_value, channel_b_value)
         logger.debug(
-            f"Rotary encoder current state: {state}, and last state: {self.last_state}"
+            f"Rotary encoder current state: {state}, last state: {self.last_state}"
         )
         last_state = self.last_state
         if state != last_state:
