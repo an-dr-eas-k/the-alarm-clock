8606b3f295033b7e1cafcf481a6fb40e5d7d4598
AndreasK <andreas.kirschner@gmail.com>
2025-10-14 22:15:09 +0200

    update setup 2

--- rpi/setup.sh
+++ rpi/setup.sh
@@ -17,10 +17,10 @@ if [ $1 != "fast" ]; then
   # apt-get -y remove python3-rpi-lgpio 
   # apt-get -y install python3-rpi.gpio
 
+  curl -sL https://dtcooper.github.io/raspotify/install.sh | sh
   apt-get -y autoremove
 fi
 
-curl -sL https://dtcooper.github.io/raspotify/install.sh | sh
 
 echo "configure system"
 ln -fs $app/rpi/resources/pigpiod.service /lib/systemd/system/pigpiod.service
