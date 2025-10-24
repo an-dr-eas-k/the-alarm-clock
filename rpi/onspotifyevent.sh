d9a77b7ca3798ea1ebeb02d65eb3b295e6772b7e
an-dr-eas-k <16319480+an-dr-eas-k@users.noreply.github.com>
2025-10-24 11:56:29 +0200

    Update systemd-cat command for Spotify event logging

--- rpi/onspotifyevent.sh
+++ rpi/onspotifyevent.sh
@@ -2,6 +2,6 @@
 
 cd /srv/the-alarm-clock/app
 python -u src/app_librespotify_event_listener.py 2>&1 \
-| systemd-cat -t the-alarm-clock.spotify-event
+| systemd-cat -u the-alarm-clock.spotify-event.service
 
 exit 0
